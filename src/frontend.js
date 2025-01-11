3f91e44629f08a36cead46e9443d7c774941be82
Le Van Quan <levanquan@192.168.1.170>
2025-01-11 14:05:41 +0700

    add 148 code

--- src/frontend.js
+++ src/frontend.js
@@ -5,14 +5,22 @@ import "./frontend.scss"
 const divsToUpdate = document.querySelectorAll(".paying-attention-update-me")
 
 divsToUpdate.forEach(function(div) {
-  ReactDOM.render(<Quiz />, div)
+  const data = JSON.parse(div.querySelector("pre").innerHTML);
+  ReactDOM.render(<Quiz {...data}/>, div)
   div.classList.remove("paying-attention-update-me")
+  // console.log("hello from react");
+  // console.log(div);
 })
 
-function Quiz() {
+function Quiz(props) {
   return (
     <div className="paying-attention-frontend">
-      Hello from React
+      {props.question}
+      <ul>
+        {props.answers.map((answer) => {
+          return <li>{answer}</li>
+        })}
+      </ul>
     </div>
   )
 }
